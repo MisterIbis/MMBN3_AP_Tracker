1f82911fbcf5d9222ee260d9feb65740231f666f
MisterIbis <107074428+MisterIbis@users.noreply.github.com>
2023-01-17 12:05:20 -0500

    Implemented AP Autotracking
    Initial implementation of AP autotracking. Will require further testing and implementation of the Secret area before release.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,16 @@
+
+ENABLE_DEBUG_LOG = true
+
+if ENABLE_DEBUG_LOG then
+    print("Debug logging is enabled!")
+end
+
 --  Load configuration options up front
 ScriptHost:LoadScript("scripts/settings.lua")
 print("Starting up scripts")
+
+-- Utility Script for helper functions etc.
+ScriptHost:LoadScript("scripts/utils.lua")
 
 -- Add Items
 Tracker:AddItems("items/keyitems.json")
@@ -17,3 +27,8 @@ Tracker:AddLayouts("layouts/tracker_standard.json")
 Tracker:AddLayouts("layouts/common.json")
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/broadcast_standard.json")
+
+-- AutoTracking for Poptracker
+if PopVersion and PopVersion >= "0.18.0" then
+    ScriptHost:LoadScript("scripts/autotracking.lua")
+end
--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,8 +1,8 @@
 function hasEnoughRanks() 
     local rankCountItem = Tracker:FindObjectForCode("ranks")
     local rankCountMax = Tracker:FindObjectForCode("ranksRequired")
-    local goalRankCount = rankCountMax.CurrentStage
-    if rankCountItem.CurrentStage >= rankShardCount then
+    local goalRankCount = 8-rankCountMax.CurrentStage
+    if rankCountItem.CurrentStage >= goalRankCount then
       return 1
     else 
       return 0
