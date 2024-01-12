c36477e50035fb91d19b83ca0821f8f6015a3512
Phantom5800 <phantom@phantom-games.com>
2024-01-12 11:13:02 -0800

    Fixes for EmoTracker support (#9)

--- scripts/init.lua
+++ scripts/init.lua
@@ -23,9 +23,9 @@ Tracker:AddMaps("maps/maps.json")
 Tracker:AddLocations("locations/locations.json")
 Tracker:AddLocations("locations/JobLocations.json")
 
-Tracker:AddLayouts("layouts/tracker_standard.json")
 Tracker:AddLayouts("layouts/common.json")
 Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/tracker_standard.json")
 Tracker:AddLayouts("layouts/broadcast_standard.json")
 
 -- AutoTracking for Poptracker
